8157a6b2536b4b3de894e080ef4fcf1f42aaeccb
Marcos Inácio <marcosincio556@gmail.com>
2025-11-23 09:49:52 -0300

    nova file

---
+++ documentos.ts
@@ -0,0 +1,20 @@
+// Interface Documento
+export interface Documento {
+    titulo: string;
+    conteudo: string;
+}
+
+// Classe Texto que implementa Documento
+export class Texto implements Documento {
+    titulo: string;
+    conteudo: string;
+
+    constructor(titulo: string, conteudo: string) {
+        this.titulo = titulo;
+        this.conteudo = conteudo;
+    }
+
+    exibir(): string {
+        return `Título: ${this.titulo}, Conteúdo: ${this.conteudo}`;
+    }
+}
